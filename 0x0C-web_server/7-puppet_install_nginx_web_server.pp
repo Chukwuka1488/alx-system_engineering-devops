98d596dce7a2e3997bfa46c148b022a379e18ebe
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-11-28 17:36:29 -0800

    puppet manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,46 +1,22 @@
 #!/usr/bin/env bash
 # Using puppet for config management
 
-class nginx {
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  file { '/var/www/html/index.nginx-debian.html':
-    ensure  => file,
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/sites-available/redirection':
-    ensure  => file,
-    content => 'server {
-      listen 80;
-      server_name localhost;
-      location / {
-        try_files $uri $uri/ =404;
-      }
-      location /redirect_me {
-        return 301 https://www.example.com;
-      }
-    }',
-    require => Package['nginx'],
-  }
-
-  exec { 'enable_redirection':
-    command => 'ln -s /etc/nginx/sites-available/redirection /etc/nginx/sites-enabled/',
-    unless  => 'test -L /etc/nginx/sites-enabled/redirection',
-    require => File['/etc/nginx/sites-available/redirection'],
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => [
-      File['/var/www/html/index.nginx-debian.html'],
-      File['/etc/nginx/sites-available/redirection'],
-    ],
-  }
+package { 'nginx':
+  ensure => installed,
 }
 
-include nginx
+file { 'install':
+  ensure  => 'present',
+  path 	  => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.github.com/chukwuka1488 permanent;',    
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}    
