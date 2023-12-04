17aab4af7751028915a693bf48983b8ef2acb438
Chukwuka Akibor <juniorakibor@yahoo.com>
2023-12-04 19:26:51 +0100

    puppet config

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,48 +1,67 @@
-c
-  package { 'nginx':
-    ensure => installed,
-  }
+# Install Nginx web server with Puppet
+include stdlib
 
-  file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
+exec { 'update packages':
+  command => '/usr/bin/apt-get update'
+}
 
-  file_line { 'nginx_server_name':
-    path => '/etc/nginx/sites-enabled/default',
-    line => "\tserver_name _;",
-    match => '^\\s*server_name',
-  }
+exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
+  require => Package['nginx'],
+}
 
-  file_line { 'nginx_redirect':
-    path => '/etc/nginx/sites-enabled/default',
-    line => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
-    after => '^\\s*server_name',
-    require => File_line['nginx_server_name'],
-  }
+package { 'nginx':
+  ensure => installed,
+  require => Exec['update packages'],
+}
 
-  file { '/var/www/html/404.html':
-    ensure  => file,
-    content => "Ceci n'est pas une page",
-    require => Package['nginx'],
-  }
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => 'Hello World!',
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+}
 
-  exec { 'add_custom_header':
-    command => "sed -i '/server_name _;/a add_header X-Served-By \$HOSTNAME;' /etc/nginx/sites-enabled/default",
-    path    => ['/bin', '/usr/bin', '/usr/sbin'],
-    require => Package['nginx'],
-  }
+file_line { 'nginx_server_name':
+  path => '/etc/nginx/sites-enabled/default',
+  line => "\tserver_name _;",
+  match => '^\\s*server_name',
+}
 
-  exec { 'nginx_test':
-    command => 'nginx -t',
-    path    => ['/bin', '/usr/bin', '/usr/sbin'],
-    require => Exec['add_custom_header'],
-  }
+file_line { 'nginx_redirect':
+  path => '/etc/nginx/sites-enabled/default',
+  line => "\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;",
+  after => '^\\s*server_name',
+  require => File_line['nginx_server_name'],
+}
 
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Exec['nginx_test'],
-  }
+file { '/var/www/html/404.html':
+  ensure  => file,
+  content => "Ceci n'est pas une page",
+  require => Package['nginx'],
+}
+
+exec { 'add_custom_header':
+  command => "sed -i '/server_name _;/a add_header X-Served-By \$HOSTNAME;' /etc/nginx/sites-enabled/default",
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  require => Package['nginx'],
+}
+
+exec { 'nginx_test':
+  command => 'nginx -t',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  require => Exec['add_custom_header'],
+}
+
+exec { 'nginx_reload':
+  command => 'nginx -s reload',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  require => Exec['nginx_test'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Exec['nginx_test'],
 }
